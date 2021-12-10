b3e49cd66f95bdc64aa4b5b9261fe583fbf5fc63
AndiezSatria <andis.permana12@gmail.com>
2021-12-10 14:01:26 +0700

    Enhance isPalindrome method

--- app/src/main/java/com/andiez/suitmediatestone/utils/Utils.kt
+++ app/src/main/java/com/andiez/suitmediatestone/utils/Utils.kt
@@ -25,21 +25,24 @@ fun convertFormatDate(oldDate: String): String {
 }
 
 fun isPalindrome(words: String): Boolean {
-    val listChar = ArrayList<Char>()
-    listChar.addAll(words.lowercase().toList())
-    listChar.removeAll { it == ' ' }
-    if (listChar.size == 1) return false
-    val firstMiddle =
-        if (listChar.size % 2 == 0) listChar.size / 2 else (listChar.size / 2) + 1
-    val secondMiddle =
-        if (listChar.size % 2 == 0) (listChar.size / 2) + 1 else (listChar.size / 2) + 1
-    val firstChars = ArrayList<Char>()
-    val secondChars = ArrayList<Char>()
-    for (i in (firstMiddle - 1) downTo 0) {
-        firstChars.add(listChar[i])
-    }
-    for (i in (secondMiddle - 1) until listChar.size) {
-        secondChars.add(listChar[i])
-    }
-    return firstChars.joinToString("") == secondChars.joinToString("")
+    return words.replace(" ", "") == words.replace(" ", "").reversed()
+
+//    Manual algorithm
+//    val listChar = ArrayList<Char>()
+//    listChar.addAll(words.lowercase().toList())
+//    listChar.removeAll { it == ' ' }
+//    if (listChar.size == 1) return false
+//    val firstMiddle =
+//        if (listChar.size % 2 == 0) listChar.size / 2 else (listChar.size / 2) + 1
+//    val secondMiddle =
+//        if (listChar.size % 2 == 0) (listChar.size / 2) + 1 else (listChar.size / 2) + 1
+//    val firstChars = ArrayList<Char>()
+//    val secondChars = ArrayList<Char>()
+//    for (i in (firstMiddle - 1) downTo 0) {
+//        firstChars.add(listChar[i])
+//    }
+//    for (i in (secondMiddle - 1) until listChar.size) {
+//        secondChars.add(listChar[i])
+//    }
+//    return firstChars.joinToString("") == secondChars.joinToString("")
 }
