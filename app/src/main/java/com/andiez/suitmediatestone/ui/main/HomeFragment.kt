44d1f9c789b26827c576859532e9534c63ebd804
AndiezSatria <andis.permana12@gmail.com>
2021-12-11 13:36:50 +0700

    Use AlertDialog for isPalindrome

--- app/src/main/java/com/andiez/suitmediatestone/ui/main/HomeFragment.kt
+++ app/src/main/java/com/andiez/suitmediatestone/ui/main/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.andiez.suitmediatestone.ui.main
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.andiez.suitmediatestone.R
 import com.andiez.suitmediatestone.databinding.FragmentHomeBinding
 import com.andiez.suitmediatestone.utils.isPalindrome
 import com.google.android.material.snackbar.Snackbar
@@ -27,14 +30,27 @@ class HomeFragment : Fragment() {
                     Snackbar.make(binding.root, "Nama tidak boleh kosong.", Snackbar.LENGTH_SHORT)
                         .show()
                 else {
-                    Toast.makeText(
-                        requireContext(),
-                        if (isPalindrome(name.trim())) "isPalindrome" else "Not Palindrome",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    this@HomeFragment.findNavController().navigate(
-                        HomeFragmentDirections.actionHomeFragmentToChooseButtonFragment(name.trim())
-                    )
+//                    Toast.makeText(
+//                        requireContext(),
+//                        if (isPalindrome(name.trim())) "isPalindrome" else "Not Palindrome",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+                    val dialog: AlertDialog = requireActivity().let {
+                        val builder = AlertDialog.Builder(it)
+                        builder.apply {
+                            setTitle("IsPalindrome ?")
+                            setMessage(if (isPalindrome(name.trim())) R.string.is_palindrome else R.string.not_palindrome)
+                            setPositiveButton("Next") { _, _ ->
+                                this@HomeFragment.findNavController().navigate(
+                                    HomeFragmentDirections.actionHomeFragmentToChooseButtonFragment(
+                                        name.trim()
+                                    )
+                                )
+                            }
+                        }
+                        builder.create()
+                    }
+                    dialog.show()
                 }
             }
         }
