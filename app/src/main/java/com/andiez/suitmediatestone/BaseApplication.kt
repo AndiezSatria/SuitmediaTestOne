0bc156f2b14f838a362a5b941353165650fbea92
AndiezSatria <andis.permana12@gmail.com>
2021-12-31 08:20:35 +0700

    Add OneSignal SDK

--- app/src/main/java/com/andiez/suitmediatestone/BaseApplication.kt
+++ app/src/main/java/com/andiez/suitmediatestone/BaseApplication.kt
@@ -2,6 +2,7 @@ package com.andiez.suitmediatestone
 
 import android.app.Application
 import com.andiez.suitmediatestone.di.Injection
+import com.onesignal.OneSignal
 import io.realm.Realm
 
 class BaseApplication : Application() {
@@ -9,5 +10,12 @@ class BaseApplication : Application() {
         super.onCreate()
         Realm.init(this)
         Realm.setDefaultConfiguration(Injection.provideRealmConfig())
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+    }
+
+    companion object {
+        private const val ONESIGNAL_APP_ID = "APP_ID"
     }
 }
